e266ae948a90b00340825d5bb2dade0215f0873e
g.georgiou <g.georgiou@dataverse.gr>
2020-11-10 13:05:24 +0200

    Start to use Formik Components such as Formik, Field, ErrorMessage

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik } from 'formik'
 import * as Yup from 'yup'
 
 const initialValues = {
@@ -47,60 +47,66 @@ const validationSchema = Yup.object({
 
 function YoutubeForm () {
 
-    //this example uses Yup for validation
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        // validate,
-        validationSchema
-    })
+    //this example uses Yup for validation (and this will be passed to Formik Component)
+    // const formik = useFormik({
+    //     initialValues,
+    //     onSubmit,
+    //     // validate,
+    //     validationSchema
+    // })
 
     // console.log('formik.touched', formik.touched)
 
     return (
-        <form onSubmit={formik.handleSubmit}>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <form>
 
-            <label htmlFor='name'>Name</label>
-            <input
-                type='text'
-                id='name'
-                name='name'
-                // onChange, onBlur, value fields
-                {...formik.getFieldProps('name')}
-            />
-            {/*formik here could also be applied to the fields that have been touched OR to some
+                <label htmlFor='name'>Name</label>
+                <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    // onChange, onBlur, value fields
+                    {...formik.getFieldProps('name')}
+                />
+                {/*formik here could also be applied to the fields that have been touched OR to some
             to improve UX (need handleBlur for that to do the touch)*/}
-            {formik.touched.name && formik.errors.name ? (
-                <p className='error'>{formik.errors.name}</p>
-            ) : null}
+                {formik.touched.name && formik.errors.name ? (
+                    <p className='error'>{formik.errors.name}</p>
+                ) : null}
 
-            <label htmlFor='email'>E-mail</label>
-            <input
-                type='text'
-                id='email'
-                name='email'
-                // onChange, onBlur, value fields
-                {...formik.getFieldProps('email')}
-            />
-            {formik.touched.email && formik.errors.email ? (
-                <p className='error'>{formik.errors.email}</p>
-            ) : null}
+                <label htmlFor='email'>E-mail</label>
+                <input
+                    type='text'
+                    id='email'
+                    name='email'
+                    // onChange, onBlur, value fields
+                    {...formik.getFieldProps('email')}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                    <p className='error'>{formik.errors.email}</p>
+                ) : null}
 
-            <label htmlFor='channel'>Channel</label>
-            <input
-                type='text'
-                id='channel'
-                name='channel'
-                // onChange, onBlur, value fields
-                {...formik.getFieldProps('channel')}
-            />
-            {formik.touched.channel && formik.errors.channel ? (
-                <p className='error'>{formik.errors.channel}</p>
-            ) : null}
+                <label htmlFor='channel'>Channel</label>
+                <input
+                    type='text'
+                    id='channel'
+                    name='channel'
+                    // onChange, onBlur, value fields
+                    {...formik.getFieldProps('channel')}
+                />
+                {formik.touched.channel && formik.errors.channel ? (
+                    <p className='error'>{formik.errors.channel}</p>
+                ) : null}
 
-            <button type='submit'>Submit</button>
-        </form>
-    )
+                <button type='submit'>Submit</button>
+            </form>
+        </Formik>
+    );
 }
 
 export default YoutubeForm;
